272ade3feddded40123f673868a95b06ef8c6f48
lrg1031@live.com <>
2021-09-30 14:58:23 -0400

    second part of our classwork

--- src/main/java/com/mycompany/week5_javafx/App.java
+++ src/main/java/com/mycompany/week5_javafx/App.java
@@ -6,7 +6,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 
@@ -15,13 +18,30 @@ public class App extends Application {
     @Override
     public void start(Stage stage) {
         
+        Rectangle r = new Rectangle();
+        r.setX(500);
+        r.setY(350);
+        r.setFill(Color.AQUA);
+        r.setWidth(100);
+        r.setHeight(100);
+        
+        Circle circle = new Circle(100, 300, 50, Color.RED);
+        
+        Rectangle r2 = new Rectangle();
+        r2.setX(0);
+        r2.setY(0);
+        r2.setFill(Color.LIGHTGREEN);
+        r2.setWidth(75);
+        r2.setHeight(25);
+        
         Button btn = new Button();
         btn.setText("Click Me");
         btn.setLayoutX(200);
         btn.setLayoutY(200);
         Label label = new Label("Hello, Luke");
+        
         Line ln = new Line(0,0, 300, 400);
-        Group qp = new Group(label, btn, ln);
+        Group qp = new Group(r2, label, btn, ln, r, circle);
         
         Scene scene = new Scene(qp, 640, 480);
         stage.setScene(scene);
